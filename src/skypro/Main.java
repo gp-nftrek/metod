9e27f07c211d121fe2492215f31b91c1ab774a1c
babk1nvnuk <babkinartem20@gmail.com>
2022-04-25 23:04:27 +0200

    6

--- src/skypro/Main.java
+++ src/skypro/Main.java
@@ -72,16 +72,14 @@ public class Main {
     }
 
     public static void reversMassiva(int[] arr) {
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = i + 1; j < arr.length-1; j++) {
-                int temp = 0;
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                System.out.print(arr[i]);
-            }
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
         }
-
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
     }
 
     public static void main(String[] args) {
